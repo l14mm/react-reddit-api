e6fe05527d13686550801b48ef39078e6b25c46e
liammoroney <liam.moroney14@gmail.com>
2019-08-22 15:36:55 +0100

    Moved access token api call to backend api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require("express");
 var router = express.Router();
+var request = require("request");
 
 router.get("/", function(req, res, next) {
-  res.json({ msg: "Hello World!" });
+  const encoded = Buffer.from(
+    `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+  ).toString("base64");
+  request.post(
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${encoded}`
+      },
+      url: "https://www.reddit.com/api/v1/access_token",
+      form: {
+        grant_type: "authorization_code",
+        code: req.query.code,
+        redirect_uri: "http://localhost:3000/redirect"
+      }
+    },
+    function(error, response, body) {
+      res.json(body);
+    }
+  );
 });
 
 module.exports = router;
