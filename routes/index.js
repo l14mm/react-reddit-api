41b1cd6a8f737d737469731d388455cd810b277f
liammoroney <liam.moroney14@gmail.com>
2020-01-24 16:20:14 +0000

    Added functionality to auto refresh access token with refresh token when access token expires

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ var express = require("express");
 var router = express.Router();
 var request = require("request");
 
+const OAUTH_API_URL = "https://oauth.reddit.com";
+const API_URL = "https://www.reddit.com/api/v1";
+
 router.get("/posts", function(req, res, next) {
   const { query, after, access_token } = req.query;
   request.get(
-    `https://oauth.reddit.com/${query}.json?after=${after}`,
+    `${OAUTH_API_URL}/${query}.json?after=${after}`,
     {
       headers: {
         Authorization: `bearer ${access_token}`,
@@ -13,6 +16,63 @@ router.get("/posts", function(req, res, next) {
       }
     },
     function(error, response, body) {
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+router.get("/refresh", function(req, res, next) {
+  const { refresh_token } = req.query;
+  const encoded = Buffer.from(
+    `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+  ).toString("base64");
+  request.post(
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${encoded}`
+      },
+      url: `${API_URL}/access_token`,
+      form: {
+        grant_type: "refresh_token",
+        refresh_token
+      }
+    },
+    function(error, response, body) {
+      const { access_token, refresh_token, expires_in } = JSON.parse(body);
+      request.get(
+        `${OAUTH_API_URL}/api/v1/me`,
+        {
+          headers: {
+            Authorization: `bearer ${access_token}`,
+            "User-Agent": "react-reddit by liam"
+          }
+        },
+        function(error, response, body) {
+          res.json({
+            ...JSON.parse(body),
+            access_token,
+            refresh_token,
+            expires_in
+          });
+        }
+      );
+    }
+  );
+});
+
+router.get("/me", function(req, res, next) {
+  const { code } = req.query;
+  request.get(
+    `${OAUTH_API_URL}/api/v1/me`,
+    {
+      headers: {
+        Authorization: `bearer ${code}`,
+        "User-Agent": "react-reddit by liam"
+      }
+    },
+    function(error, response, body) {
+      console.log("**me", JSON.parse(body));
       res.json(JSON.parse(body));
     }
   );
@@ -28,7 +88,7 @@ router.get("/", function(req, res, next) {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization: `Basic ${encoded}`
       },
-      url: "https://www.reddit.com/api/v1/access_token",
+      url: `${API_URL}/access_token`,
       form: {
         grant_type: "authorization_code",
         code: req.query.code,
@@ -38,7 +98,7 @@ router.get("/", function(req, res, next) {
     function(error, response, body) {
       const { access_token, refresh_token, expires_in } = JSON.parse(body);
       request.get(
-        "https://oauth.reddit.com/api/v1/me",
+        `${OAUTH_API_URL}/api/v1/me`,
         {
           headers: {
             Authorization: `bearer ${access_token}`,
@@ -46,6 +106,12 @@ router.get("/", function(req, res, next) {
           }
         },
         function(error, response, body) {
+          console.log("**me1st", {
+            ...JSON.parse(body),
+            access_token,
+            refresh_token,
+            expires_in
+          });
           res.json({
             ...JSON.parse(body),
             access_token,
